4cb3a0125dc521b0668f15ec3a9105869e28c0b0
vinay krishna <vinay.krishna@team.cliffex.com>
2021-09-23 13:03:55 +0530

    image scalton

--- component/custom-image.js
+++ component/custom-image.js
@@ -20,7 +20,7 @@ const CustomImage = ({ src, alt, className, height, width, onClick, noSkeleton})
                 <>
                     {/* <div className="d-none"><img src={src} alt={alt} className={className} onClick={onClick} onLoad={onLoad}/></div> */}
                 
-                        {!loading ? <span className={className} style={{overflow: "hidden"}}><Skeleton height={height} width={width} /></span> : null}
+                        {loading ? <span className={className} style={{overflow: "hidden"}}><Skeleton height={height} width={width} /></span> : null}
                        
                         <img src={src} alt={alt} className={className, loading ? `d-none` : ``} onClick={onClick} onLoad={onLoad}/>
                  
